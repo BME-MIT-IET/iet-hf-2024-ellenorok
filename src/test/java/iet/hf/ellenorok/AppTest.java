614061dd95ed7cf47befd704994bfc410600d23e
bozsoxd <nagybozso12@gmail.com>
2024-05-11 14:01:45 +0200

    test junit test

--- src/test/java/iet/hf/ellenorok/AppTest.java
+++ src/test/java/iet/hf/ellenorok/AppTest.java
@@ -1,20 +1,46 @@
 package iet.hf.ellenorok;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+ 
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+         void Test() {
+        // create fields
+        Pipe pipe1 = new Pipe();
+        //create characters
+        Saboteur saboteur1 = new Saboteur();
+
+        // connect components
+        //add players to a field
+        saboteur1.setActiveField(pipe1); pipe1.addPlayer(saboteur1);
+
+        // create game
+        Game g = Game.getInstance();
+        g.setActivePlayer(saboteur1);
+        //add fields to game
+        g.addField(pipe1);
+        //add players to game
+        g.addPlayer(saboteur1);
+
+        // add periodics to timer
+        Timer.getInstance().addPeriodic(pipe1);
+        //add statefuls to timer
+        Timer.getInstance().addStateful(pipe1);
+
+        //kimenethez ezt ki kell kommentelni
+        //
+        saboteur1.breakField();
+        
+        assertEquals(true, pipe1.isBroken());
+        
     }
 }
