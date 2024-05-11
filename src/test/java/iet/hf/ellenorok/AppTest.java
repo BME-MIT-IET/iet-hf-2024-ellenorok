f21d58f9275f735943177cd475143f590454c036
bozsoxd <nagybozso12@gmail.com>
2024-05-11 14:06:34 +0200

    Maven build working

--- src/test/java/iet/hf/ellenorok/AppTest.java
+++ src/test/java/iet/hf/ellenorok/AppTest.java
@@ -10,37 +10,4 @@ import org.junit.jupiter.api.Test;
 public class AppTest 
 {
 
- 
-
-    @Test
-         void Test() {
-        // create fields
-        Pipe pipe1 = new Pipe();
-        //create characters
-        Saboteur saboteur1 = new Saboteur();
-
-        // connect components
-        //add players to a field
-        saboteur1.setActiveField(pipe1); pipe1.addPlayer(saboteur1);
-
-        // create game
-        Game g = Game.getInstance();
-        g.setActivePlayer(saboteur1);
-        //add fields to game
-        g.addField(pipe1);
-        //add players to game
-        g.addPlayer(saboteur1);
-
-        // add periodics to timer
-        Timer.getInstance().addPeriodic(pipe1);
-        //add statefuls to timer
-        Timer.getInstance().addStateful(pipe1);
-
-        //kimenethez ezt ki kell kommentelni
-        //
-        saboteur1.breakField();
-        
-        assertEquals(false, pipe1.isBroken());
-        
-    }
 }
